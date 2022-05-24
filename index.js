623214ab8cdcbd3dc65d7fcf3b89c04844ce1507
Christopher Callender <christophercallender0000@gmail.com>
2022-05-24 13:44:01 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 class Formatter {
-  //add static methods here
+   static capitalize(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+   }
+
+   static sanitize(string) {
+      return string.replace(/[^A-Za-z0-9-' ]+/g, '');
+   }
+
+   static titleize(string) {
+      let alwaysLowerCase = [
+         'the',
+         'a',
+         'an',
+         'but',
+         'of',
+         'and',
+         'for',
+         'at',
+         'by',
+         'from',
+      ];
+      return string
+         .split(' ')
+         .map((word, index) => {
+            return index != 0 && alwaysLowerCase.includes(word)
+               ? this.sanitize(word)
+               : this.capitalize(word);
+         })
+         .join(' ');
+   }
 }
